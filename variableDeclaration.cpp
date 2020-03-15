758980ab9fe6f148a80fe96d4838c402832824b1
Safin Singh <42120904+safinsingh@users.noreply.github.com>
2020-03-15 10:07:28 -0700

    Create variableDeclaration.cpp

---
+++ variableDeclaration.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int a = 5;
+    float b = 3.4;
+    double c = 9.745;
+    char d = 'z';
+    bool e = true;
+
+    cout << "Value of Integer is: ";
+    cout << a << endl;
+
+    cout << "Value of Float is: ";
+    cout << b << endl;
+
+    cout << "Value of Double is: " ;
+    cout << c << endl;
+
+    cout << "Value of Char is: ";
+    cout << d << endl;
+
+    cout << "Value of Bool is: ";
+    cout << e << endl;
+
+    return 0;
+}
