ac2cb23efdfcc9082e989917e15c72b407747d48
Safin Singh <42120904+safinsingh@users.noreply.github.com>
2020-03-15 10:15:41 -0700

    Create calculator.cpp

---
+++ calculator.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+float test(float num1, float num2,char Operator)
+{
+    
+
+    float temp; 
+    cout << "Number 1 is " << num1 << endl;
+    cout << "Number 2 is " << num2 << endl;
+    cout << "Operator is: " <<Operator<<endl;
+  
+
+    switch(Operator)
+    {
+    case '+':
+        temp = num1+num2;
+        cout << " Number 1 + Number 2 = " << temp;
+        break;
+
+    case '-':
+        temp = num1-num2;
+        cout << " Number 1 - Number 2 = " << temp;
+        break;
+
+    case '*':
+        temp = num1*num2;
+        cout << " Number 1 * Number 2 = " << temp;
+        break;
+
+    case '/':
+         temp = num1/num2;
+         cout << " Number 1 / Number 2 = " << temp;
+         break;
+
+    default:
+        cout << "You have entered the wrong operator";
+        break;
+    }
+  return temp;
+}
